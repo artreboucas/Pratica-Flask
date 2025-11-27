b9b048ec2e9d2fdd6f9614469cc973467c427596
Arthur Rebouças <arthurreboucas123@gmail.com>
2025-11-27 11:23:42 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,24 @@
-from flask import Flask, request, render_template
+from flask import Flask, request
 
-app = Flask(__name__, template_folder='.')
+app = Flask(__name__)
 
-# Questão 1 - GET com variáveis na rota
+# Questão 1 - GET 
 @app.get("/soma/<int:num1>/<int:num2>")
 def somar_get(num1, num2):
     return f"A soma é {num1 + num2}"
 
-# Questão 1 - POST recebendo JSON
+# Questão 1 - POST 
 @app.post("/soma")
 def somar_post():
-    data = request.json
-    num1 = data.get("num1")
-    num2 = data.get("num2")
-    return {"resultado": num1 + num2}
+    num1 = float(request.form.get("num1"))
+    num2 = float(request.form.get("num2"))
+    return f"A soma é {num1 + num2}"
 
-# Questão 2 - GET usando query params
-@app.get("/soma_query")
+# Questão 2 - GET 
+@app.get("/soma2")
 def somar_query():
     num1 = request.args.get("num1", type=float)
     num2 = request.args.get("num2", type=float)
-    if num1 is None or num2 is None:
-        return "Use: /soma_query?num1=10&num2=20"
     return f"A soma é {num1 + num2}"
 
 if __name__ == "__main__":
